d189af75636a0d3970d2c6e709bb0a8835640069
David Burnette <david.l.burnette@gmail.com>
2026-06-16 21:45:49 -0500

    Initial Commit

---
+++ Witness/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Witness
+//
+//  Created by David Burnette on 6/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Witness/WitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  WitnessApp.swift
+//  Witness
+//
+//  Created by David Burnette on 6/16/26.
+//
+
+import SwiftUI
+
+@main
+struct WitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
